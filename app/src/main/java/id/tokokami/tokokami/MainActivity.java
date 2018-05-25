2571df797ecd478c390801efe3005d21503ad5cb
Mughir <muhghan@gmail.com>
2018-05-25 18:18:34 +0700

    update

--- app/src/main/java/id/tokokami/tokokami/MainActivity.java
+++ app/src/main/java/id/tokokami/tokokami/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         }
         if (savedInstanceState == null)
         {
-            mWebView.loadUrl("https://mobile.tokokami.id");
+            mWebView.loadUrl("http://mughir.tech");
         }
 
     }
